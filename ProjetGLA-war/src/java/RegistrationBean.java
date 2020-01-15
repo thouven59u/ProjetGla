917004330df281c601efce76db73ba2d6b87ffb0
Quentin Thouv <quentin.thouvenot8@etu.univ-lorraine.fr>
2020-01-15 16:03:23 +0100

    Init

---
+++ ProjetGLA-war/src/java/RegistrationBean.java
@@ -0,0 +1,39 @@
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author quent
+ */
+
+@Named(value = "registrationBean")
+@RequestScoped
+public class RegistrationBean {
+    private String login, password;
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    public String registerLogin() {
+        return "index";
+    }
+}
